8870f1d78e84fd0aefb1278f7f1003e8774ab764
Ryna Truong <rynatruong@outlook.com>
2020-06-24 18:01:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
+<<<<<<< HEAD
   new_arr = []
   array.each do |num|
    new_arr << num * num
+=======
+  new_arr = array.each do |num|
+    num * num
+>>>>>>> 0a8e4ed3bb057c6a41a3cfb55b69dbcd8f0702d8
   end
   return new_arr
 end
